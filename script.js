a7ac9ac6a3d8eb616db3f593cc618652fdc9d55b
TrungBui-Purdue-5913 <bui23@purdue.edu>
2021-09-16 12:01:41 -0400

    index.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let first_varr = "First Variable"
+const second_varr = "Second Variable"
+var third_varr = "Third Variable"
+
+//console.log(first_varr)
+//console.log(second_varr)
+//console.log(third_varr)
