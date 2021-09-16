53c20d1361792e17dab30df1dd112600c82af1be
TrungBui-Purdue-5913 <bui23@purdue.edu>
2021-09-16 14:49:49 -0400

    final

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var third_varr = "Third Variable"
 
 // Js doesnt type oriented
 const movie_cast = [ "Robert Downey Jr", "Chris Hemsworth", "Mark Ruffalo"]
+// For each function:
+movie_cast.forEach((x) => console.log(x))
+// Map:
+const number = [2, 3, 4]
+const sqrt_number = number.map((x) => x*x)
+
+sqrt_number.forEach((x) => console.log(x))
+
+
 //console.log(first_varr)
 //console.log(second_varr)
 //console.log(third_varr)
