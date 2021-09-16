73c642976236ac76846671d2cba3d23611361532
TrungBui-Purdue-5913 <bui23@purdue.edu>
2021-09-16 12:07:09 -0400

    add cast

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let first_varr = "First Variable"
 const second_varr = "Second Variable"
 var third_varr = "Third Variable"
 
+// Js doesnt type oriented
+const movie_cast = [ "Robert Downey Jr", "Chris Hemsworth", "Mark Ruffalo"]
 //console.log(first_varr)
 //console.log(second_varr)
 //console.log(third_varr)
